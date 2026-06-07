9ebb20dd74bf8a21fb9efccb182a07ccadb97318
lorenzomadiai <s4069838@vuw.leidenuniv.nl>
2026-06-07 23:47:46 +0200

    small changes

--- code/proposed_method/utils/__init__.py
+++ code/proposed_method/utils/__init__.py
@@ -2,5 +2,5 @@
 
 from .data_utils import split_supervised_dataset, classification_metrics, load_episode_pool_csv
 from .sampling import collect_dataset, collect_gap_episode
-from .evaluation import evaluate_gap_policy, evaluate_fixed_policy
+from .evaluation import evaluate_gap_policy
 from .training import train_classifier
